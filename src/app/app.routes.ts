dc907e749fe1660ede42732ffc3ce87a5013e7c0
Allan Lemes <aflemess@gmail.com>
2023-12-03 10:03:07 -0300

    init commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SimpleFormComponent } from './simple-form/simple-form.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'simple-form', component: SimpleFormComponent}
+];
